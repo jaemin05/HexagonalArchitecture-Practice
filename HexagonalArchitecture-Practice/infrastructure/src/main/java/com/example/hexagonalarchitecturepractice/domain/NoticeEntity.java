3d9310b60c7b055f56240d7c518cb5a9bc0e0742
jaemin <sonjm3518@dsm.hs.kr>
2022-06-24 10:59:38 +0900

    📑 :: (#5) Notice Entity

---
+++ HexagonalArchitecture-Practice/infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/NoticeEntity.java
@@ -0,0 +1,23 @@
+package com.example.hexagonalarchitecturepractice.domain;
+
+import com.example.hexagonalarchitecturepractice.global.BaseEntity;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "tbl_notice")
+@Entity
+public class NoticeEntity extends BaseEntity {
+
+    @Column(length = 50 ,nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+}
