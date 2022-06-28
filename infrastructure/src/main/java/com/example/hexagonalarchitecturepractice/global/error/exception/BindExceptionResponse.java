9488936f959811dd5df28be473ee40e411d3180f
jaemin <sonjm3518@dsm.hs.kr>
2022-06-28 21:22:52 +0900

    ⚡ :: @Getter 제거

--- infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/global/error/exception/BindExceptionResponse.java
+++ infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/global/error/exception/BindExceptionResponse.java
@@ -1,15 +1,10 @@
 package com.example.hexagonalarchitecturepractice.global.error.exception;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.util.Map;
 
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public record BindExceptionResponse(HttpStatus status, Map<String, String> messages) {
 }
