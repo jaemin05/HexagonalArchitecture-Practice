09848bcccdfdf34bcf5e75e1279e30c1076ce5bd
jaemin <sonjm3518@dsm.hs.kr>
2022-06-30 12:02:34 +0900

    📑 :: (#19) Create Notice Request

---
+++ infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/notice/presentation/dto/CreateNoticeRequest.java
@@ -0,0 +1,19 @@
+package com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class CreateNoticeRequest {
+
+    @NotBlank(message = "title은 필수 입력입니다.")
+    @Size(min = 1, max = 50, message = "title은 1~30자리 이내로 입력해주세요.")
+    private String title;
+
+    @NotBlank(message = "content은 필수 입력입니다.")
+    private String content;
+}
