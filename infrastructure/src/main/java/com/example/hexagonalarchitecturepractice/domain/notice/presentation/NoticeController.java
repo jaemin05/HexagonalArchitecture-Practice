e5bf14d558f1790fd4a85a79397922f7f1b82bbb
jaemin <sonjm3518@dsm.hs.kr>
2022-07-01 18:26:58 +0900

    ⚡ :: (#21) reviseNotice Controller

--- infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/notice/presentation/NoticeController.java
+++ infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/notice/presentation/NoticeController.java
@@ -1,13 +1,15 @@
 package com.example.hexagonalarchitecturepractice.domain.notice.presentation;
 
-import com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto.CreateNoticeRequest;
+import com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto.NoticeRequest;
 import com.example.hexagonalarchitecturepractice.notice.api.CreateNoticeApi;
-import com.example.hexagonalarchitecturepractice.notice.api.dto.CreateNoticeDomainRequest;
+import com.example.hexagonalarchitecturepractice.notice.api.ReviseNoticeApi;
+import com.example.hexagonalarchitecturepractice.notice.api.dto.NoticeDomainRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/notice")
@@ -15,15 +17,28 @@ import javax.validation.Valid;
 public class NoticeController {
 
     private final CreateNoticeApi createNoticeApi;
+    private final ReviseNoticeApi reviseNoticeApi;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createNotice(@RequestBody @Valid CreateNoticeRequest request) {
-        CreateNoticeDomainRequest domainRequest = CreateNoticeDomainRequest.builder()
+    public void createNotice(@RequestBody @Valid NoticeRequest request) {
+
+        NoticeDomainRequest domainRequest = builderToDomainRequest(request);
+        createNoticeApi.createNotice(domainRequest);
+    }
+
+    @PutMapping("/{notice-uuid}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void reviseNotice(@PathVariable("notice-uuid") UUID noticeId, @RequestBody @Valid NoticeRequest request) {
+
+        NoticeDomainRequest domainRequest = builderToDomainRequest(request);
+        reviseNoticeApi.reviseNotice(noticeId, domainRequest);
+    }
+
+    private NoticeDomainRequest builderToDomainRequest(NoticeRequest request) {
+        return NoticeDomainRequest.builder()
                 .title(request.getTitle())
                 .content(request.getContent())
                 .build();
-
-        createNoticeApi.createNotice(domainRequest);
     }
 }
