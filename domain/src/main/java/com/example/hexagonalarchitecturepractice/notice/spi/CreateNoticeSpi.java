cddb7fc726d6c62b9f01ab06dfabcf6ff51cff4c
jaemin <sonjm3518@dsm.hs.kr>
2022-07-01 18:28:59 +0900

    📑 :: (#21) Create Notice Spi

---
+++ domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/spi/CreateNoticeSpi.java
@@ -0,0 +1,8 @@
+package com.example.hexagonalarchitecturepractice.notice.spi;
+
+import com.example.hexagonalarchitecturepractice.notice.Notice;
+
+public interface CreateNoticeSpi {
+
+    void createNotice(Notice notice);
+}
