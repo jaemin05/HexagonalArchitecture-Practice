12900aa91cf3deb7972e906cc76fb9b81b53b22d
jaemin <sonjm3518@dsm.hs.kr>
2022-07-03 23:37:42 +0900

    ⚡ :: (#23) queryNoticeList 추가

--- infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/notice/presentation/NoticeController.java
+++ infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/notice/presentation/NoticeController.java
@@ -1,9 +1,11 @@
 package com.example.hexagonalarchitecturepractice.domain.notice.presentation;
 
-import com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto.NoticeRequest;
+import com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto.request.NoticeRequest;
+import com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto.response.NoticeListResponse;
 import com.example.hexagonalarchitecturepractice.notice.api.CreateNoticeApi;
+import com.example.hexagonalarchitecturepractice.notice.api.QueryNoticeApi;
 import com.example.hexagonalarchitecturepractice.notice.api.ReviseNoticeApi;
-import com.example.hexagonalarchitecturepractice.notice.api.dto.NoticeDomainRequest;
+import com.example.hexagonalarchitecturepractice.notice.api.dto.request.NoticeDomainRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ public class NoticeController {
 
     private final CreateNoticeApi createNoticeApi;
     private final ReviseNoticeApi reviseNoticeApi;
+    private final QueryNoticeApi queryNoticeApi;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -33,6 +36,11 @@ public class NoticeController {
 
         NoticeDomainRequest domainRequest = builderToDomainRequest(request);
         reviseNoticeApi.reviseNotice(noticeId, domainRequest);
+    }
+
+    @GetMapping
+    public NoticeListResponse queryNoticeList() {
+        return new NoticeListResponse(queryNoticeApi.queryNotice());
     }
 
     private NoticeDomainRequest builderToDomainRequest(NoticeRequest request) {
