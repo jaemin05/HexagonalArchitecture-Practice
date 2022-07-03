1538bb20c75fff5bcc01aa14289dd76a148bac40
jaemin <sonjm3518@dsm.hs.kr>
2022-07-03 23:37:59 +0900

    ♻️ :: 폴더 위치 변경

--- infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/notice/presentation/dto/request/NoticeRequest.java
+++ infrastructure/src/main/java/com/example/hexagonalarchitecturepractice/domain/notice/presentation/dto/request/NoticeRequest.java
@@ -1,4 +1,4 @@
-package com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto;
+package com.example.hexagonalarchitecturepractice.domain.notice.presentation.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
