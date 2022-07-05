754718b29878be50d314f2f7b481288f2e8da08c
jaemin <sonjm3518@dsm.hs.kr>
2022-07-05 15:28:38 +0900

    ♻️ :: (#37) reviseNotice → reviseNoticeApi

--- domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/api/ReviseNoticeApi.java
+++ domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/api/ReviseNoticeApi.java
@@ -6,5 +6,5 @@ import java.util.UUID;
 
 public interface ReviseNoticeApi {
 
-    void reviseNotice(UUID noticeId, NoticeDomainRequest request);
+    void reviseNoticeApi(UUID noticeId, NoticeDomainRequest request);
 }
