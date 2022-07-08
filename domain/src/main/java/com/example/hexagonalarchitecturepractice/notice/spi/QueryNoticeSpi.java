9e8103e4c4c203d05cf1b4d21d54d451ae4267df
jaemin <sonjm3518@dsm.hs.kr>
2022-07-08 22:06:51 +0900

    ⚡ :: (#41) @Spi 추가

--- domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/spi/QueryNoticeSpi.java
+++ domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/spi/QueryNoticeSpi.java
@@ -1,9 +1,11 @@
 package com.example.hexagonalarchitecturepractice.notice.spi;
 
+import com.example.hexagonalarchitecturepractice.annotation.Spi;
 import com.example.hexagonalarchitecturepractice.notice.api.dto.response.NoticeDomainResponse;
 
 import java.util.List;
 
+@Spi
 public interface QueryNoticeSpi {
 
     List<NoticeDomainResponse> findAll();
--- domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/spi/ReviseNoticeSpi.java
+++ domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/spi/ReviseNoticeSpi.java
@@ -1,9 +1,11 @@
 package com.example.hexagonalarchitecturepractice.notice.spi;
 
+import com.example.hexagonalarchitecturepractice.annotation.Spi;
 import com.example.hexagonalarchitecturepractice.notice.api.dto.request.NoticeDomainRequest;
 
 import java.util.UUID;
 
+@Spi
 public interface ReviseNoticeSpi {
 
     void reviseNotice(UUID noticeId, NoticeDomainRequest request);
