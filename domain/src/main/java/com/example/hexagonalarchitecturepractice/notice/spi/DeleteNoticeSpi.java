1d1efcac90d516084fdb4346440113092ceddc46
jaemin <sonjm3518@dsm.hs.kr>
2022-07-08 22:06:51 +0900

    ⚡ :: (#42) @Spi 추가

--- domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/spi/DeleteNoticeSpi.java
+++ domain/src/main/java/com/example/hexagonalarchitecturepractice/notice/spi/DeleteNoticeSpi.java
@@ -1,7 +1,10 @@
 package com.example.hexagonalarchitecturepractice.notice.spi;
 
+import com.example.hexagonalarchitecturepractice.annotation.Spi;
+
 import java.util.UUID;
 
+@Spi
 public interface DeleteNoticeSpi {
 
     void deleteNotice(UUID noticeId);
